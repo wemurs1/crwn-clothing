16253fcb6ae6f595492e3ab99075ddf68ef28b8b
Stuart Murray <stuart.murray.dev@outlook.com>
2023-06-28 17:17:52 +0100

    use reducer in categories context

--- src/contexts/categories.context.tsx
+++ src/contexts/categories.context.tsx
@@ -1,16 +1,57 @@
-import { ReactNode, createContext, useEffect, useState } from 'react';
+import {
+  ReactNode,
+  createContext,
+  useEffect,
+  useReducer,
+} from 'react';
 import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+import { createAction } from '../utils/reducer/reducer.utils';
 
 interface Props {
   children: ReactNode;
+}
+
+interface ActionProps {
+  type: string;
+  payload: any;
 }
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
+export const CATEGORIES_ACTION_TYPES = {
+  SET_CATEGORIES: 'SET_CATEGORIES',
+};
+
+const INITIAL_STATE = {
+  categoriesMap: {},
+};
+
+const categoriesReducer = (state: any, action: ActionProps) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case CATEGORIES_ACTION_TYPES.SET_CATEGORIES:
+      return {
+        categoriesMap: payload,
+      };
+
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
 export const CategoriesProvider = ({ children }: Props) => {
-  const [categoriesMap, setCategoriesMap] = useState({});
+  const [state, dispatch] = useReducer(categoriesReducer, INITIAL_STATE);
+
+  const { categoriesMap } = state;
+
+  const setCategoriesMap = (categoriesMap: any) => {
+    dispatch(
+      createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesMap)
+    );
+  };
 
   useEffect(() => {
     const getCategoriesMap = async () => {
